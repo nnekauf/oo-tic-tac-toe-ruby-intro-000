96a5ab46a0b34feded0ea1715765095049c9eea9
Nneka U <ufumakanneka@gmail.com>
2020-11-05 14:21:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,7 +34,7 @@ def position_taken?(location)
   @board[location] != " " && @board[location] != ""
 end
 
-def valid_move?( index)
+def valid_move?(index)
   index.between?(0,8) && !position_taken?(@board, index)
 end
 

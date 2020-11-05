facb58e1496424e2f011fea51e52581f4c05a8d7
Nneka U <ufumakanneka@gmail.com>
2020-11-05 14:26:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,9 +75,9 @@ def won?
     win_index_3 = win_combination[2]
     
     
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
+    position_1 = @board[win_index_1]
+    position_2 = @board[win_index_2]
+    position_3 = @board[win_index_3]
     
     
       if (position_1== "X" && position_2 == "X" && position_3 == "X")

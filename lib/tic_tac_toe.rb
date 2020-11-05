95072804cc1b3ee1d6a74bc3f2ae56ff5f74ce62
Nneka U <ufumakanneka@gmail.com>
2020-11-05 14:12:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,7 +3,14 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
   
-  def WIN_COMBINATIONS 
-    
-  end
+  WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5], 
+  [6,7,8],
+  [0,4,8],
+  [6,4,2],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
 end
